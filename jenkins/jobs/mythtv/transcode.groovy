78da400abf45356213dc21deec42db6ae5edf93f
gitdaniele <dany4any@gmail.com>
2025-12-22 08:37:56 +0100

    fix classname

--- jenkins/jobs/mythtv/transcode.groovy
+++ jenkins/jobs/mythtv/transcode.groovy
@@ -39,7 +39,7 @@ pipeline {
         stage('Get Storage Group Videos') {
             steps {
                 script {
-                    storageDirs = mythStorage(
+                    storageDirs = mythtvStorage(
                         baseUrl: MYTH_BASEURL,
                         action: 'list',
                         groupName: GROUPNAME,
