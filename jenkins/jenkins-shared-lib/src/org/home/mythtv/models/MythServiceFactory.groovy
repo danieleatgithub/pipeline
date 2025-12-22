da31867a59a9f4962a23969412d4703a6d1b9daf
gitdaniele <dany4any@gmail.com>
2025-12-22 11:56:56 +0100

    fix signature

--- jenkins/jenkins-shared-lib/src/org/home/mythtv/models/MythServiceFactory.groovy
+++ jenkins/jenkins-shared-lib/src/org/home/mythtv/models/MythServiceFactory.groovy
@@ -4,14 +4,16 @@ class MythServiceFactory implements Serializable {
 
     def steps
     String baseUrl
+    private final HttpClient client
 
     MythServiceFactory(steps, String baseUrl) {
         this.steps = steps
         this.baseUrl = baseUrl
+        this.client = new HttpClient(steps, baseUrl)
     }
 
     MythStorageService storageService() {
-        return new MythStorageService(steps, baseUrl)
+        return new MythStorageService(client)
     }
 
 }
