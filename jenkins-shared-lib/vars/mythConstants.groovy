efaaae5fa74ee6cb6fb9d6f0985578886940f74f
gitdaniele <dany4any@gmail.com>
2025-12-28 10:29:14 +0100

    fix CPS

--- jenkins-shared-lib/vars/mythConstants.groovy
+++ jenkins-shared-lib/vars/mythConstants.groovy
@@ -1,5 +1,10 @@
-def call() {
+@NonCPS
+def getConstants() {
     return [
             VIDEO_GROUPNAME: org.home.myth.Constants.VIDEO_GROUPNAME
     ]
+}
+
+def call() {
+    return getConstants()
 }
