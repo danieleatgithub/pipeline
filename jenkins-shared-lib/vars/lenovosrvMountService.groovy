6ed434d50c463bc0e85a58819b3ec7034f446818
gitdaniele <dany4any@gmail.com>
2025-12-28 11:53:45 +0100

    refactor

--- jenkins-shared-lib/vars/lenovosrvMountService.groovy
+++ jenkins-shared-lib/vars/lenovosrvMountService.groovy
@@ -1,37 +1,3 @@
-import org.home.lenovosrv.models.LenovosrvServiceFactory
-
 def call(Map args = [:]) {
-
-    String baseUrl     = args.baseUrl   ?: error("baseUrl obbligatorio")
-    String action      = args.action    ?: error("action obbligatoria")
-    String mountTarget = args.mountTarget
-    String fstype      = args.fstype
-
-    def factory = new LenovosrvServiceFactory(
-        this,
-        baseUrl
-    )
-
-    def mountSvc = factory.mountService()
-
-    switch(action) {
-
-        case 'status':
-            return mountSvc.status()
-
-        case 'mount':
-            if (!mountTarget) error "mountTarget obbligatorio per mount"
-            return mountSvc.mount(mountTarget)
-
-        case 'umount':
-            if (!mountTarget) error "mountTarget obbligatorio per umount"
-            return mountSvc.umount(mountTarget)
-
-        case 'listByFsType':
-            if (!fstype) error "fstype obbligatorio per listByFsType"
-            return mountSvc.getMountsByFsType(fstype)
-
-        default:
-            error "action non supportata: ${action}"
-    }
+    new org.home.lenovosrv.pipeline.LenovosrvMountService(this).run(args)
 }
