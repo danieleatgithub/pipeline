ec900a329d75418cb94c52f38ef8d12a2835b7ac
gitdaniele <dany4any@gmail.com>
2026-01-02 08:10:36 +0100

    draft

--- jenkins-shared-lib/src/org/home/models/HelloWorld.groovy
+++ jenkins-shared-lib/src/org/home/models/HelloWorld.groovy
@@ -1,9 +1,12 @@
 package org.home.models
 
+//import com.cloudbees.groovy.cps.NonCPS
+
 class HelloWorld implements Serializable {
 
     private final def steps
-    private final String baseHelloString
+    private String baseHelloString
+    private String hello
 
     HelloWorld(steps, String helloString = "Hello ") {
         this.steps = steps
@@ -17,12 +20,30 @@ class HelloWorld implements Serializable {
     }
     String getHello() {
         steps.echo "[HelloWorld] getHello"
-        return this.baseHelloString + this.hello
+        return formatHello()
     }
     HelloWorld setHello(String helloString) {
         this.hello = helloString
         return this
     }
+
+    // ---- LOGICA PURA (NO CPS) ----
+    @NonCPS
+    private String formatHello() {
+        return baseHelloString + hello
+    }
+
+    /*
+    Cos’è CPS in Jenkins
+            CPS = Continuation Passing Style
+    In Jenkins Pipeline ogni step può fermare il job (echo, sh, input, sleep, ecc.).
+    Jenkins quindi:
+    * salva lo stato del programma
+    * termina l’esecuzione
+    * riprende da lì quando lo step finisce (o dopo un restart di Jenkins)
+
+    Per farlo, Jenkins trasforma il tuo Groovy in una macchina a stati.
+    */
 
 }
 
