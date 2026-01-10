df3a87a9ab244f368bad1495c8e366ac3eb213bb
gitdaniele <dany4any@gmail.com>
2026-01-10 11:07:15 +0100

    draft job for unit test

---
+++ jenkins/jobs/develop/jenkins-shared-libs-unit.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent any
+
+    options {
+        timestamps()
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Unit Tests') {
+            steps {
+                sh '''
+                  docker compose build jenkins-shared-lib-test
+                  docker compose run --rm jenkins-shared-lib-test
+                '''
+            }
+        }
+    }
+
+    post {
+        always {
+            junit 'build/test-results/test/**/*.xml'
+        }
+        cleanup {
+            sh 'docker compose down -v || true'
+        }
+    }
+}
